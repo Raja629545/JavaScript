639d0f258578702013f297fa3735bd8ecaaf45a6
Raja629545 <rajesh713428@gmail.com>
2023-09-22 11:35:57 +0530

    control flow

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -21,22 +21,22 @@ const temperature = 41
 // console.log(`User power: ${power}`);
 
 
-// const balance = 1000
+const balance = 1000
 
-// if (balance > 500) console.log("test"),console.log("test2");
+if (balance > 500) console.log("test"),console.log("test2");
 
-// if (balance < 500) {
-//     console.log("less than 500");
-// } else if (balance < 750) {
-//     console.log("less than 750");
+if (balance < 500) {
+    console.log("less than 500");
+} else if (balance < 750) {
+    console.log("less than 750");
     
-// } else if (balance < 900) {
-//     console.log("less than 750");
+} else if (balance < 900) {
+    console.log("less than 750");
     
-// } else {
-//     console.log("less than 1200");
+} else {
+    console.log("less than 1200");
 
-// }
+}
 
 const userLoggedIn = true
 const debitCard = true

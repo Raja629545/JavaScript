8166bb3dd0d5d3bda060533a91ffd6117d8a6a45
Raja629545 <rajesh713428@gmail.com>
2023-09-23 00:40:29 +0530

    filter, map, reduce in js

---
+++ 05_iterations/eight.js
@@ -0,0 +1,33 @@
+const myNum = [1, 2, 3]
+
+// const myTotal = myNum.reduce(function (acc, currVal) {
+//     console.log(`acc is ${acc} and currval is ${currVal}`);
+//     return acc + currVal;
+// }, 7)
+
+const myTotal = myNum.reduce((acc, currval) => ( acc + currval), 0)
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);

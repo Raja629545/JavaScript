2aa69bbfd1a2d56861a00372c2fabb538c01a73e
Raja629545 <rajesh713428@gmail.com>
2023-10-05 23:38:43 +0530

    peomises with fetch notes

--- 09_advance/promises.js
+++ 09_advance/promises.js
@@ -102,3 +102,12 @@ fetch('https://api.github.com/users/Raja629545')
     console.log(data);
 })
 .catch((error) => console.log(error))
+
+// About Fetch()
+
+// The global fetch() method starts the process of fetching a resource from the network, returning a promise which is fulfilled once the response is available.
+
+// The promise resolves to the Response object representing the response to your request.
+
+// A fetch() promise only rejects when a network error is encountered (which is usually when there's a permissions issue or similar). A fetch() promise does not reject on HTTP errors (404, etc.). Instead, a then() handler must check the Response.ok and/or Response.status properties.
+

6de33e8082567e751ba57e7c16b98704d3a5eeee
Raja629545 <rajesh713428@gmail.com>
2023-10-06 12:13:13 +0530

    myClasses in js

---
+++ 10_classes_and_oop/myClasses.js
@@ -0,0 +1,39 @@
+
+class user {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+    changeUserName(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+const Raja = new user("Rajesh", "Rajesh@gmail.com", "234")
+
+console.log(Raja.encryptPassword());
+console.log(Raja.changeUserName());
+
+// behind the scenes
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());

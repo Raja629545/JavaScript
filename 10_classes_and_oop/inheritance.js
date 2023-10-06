4eb3e564b7e44b64e64db42ef7d27a14806393c0
Raja629545 <rajesh713428@gmail.com>
2023-10-06 12:26:55 +0530

    Inheritance in js

---
+++ 10_classes_and_oop/inheritance.js
@@ -0,0 +1,31 @@
+class user{
+    constructor(username){
+        this.username = username;
+    }
+
+    logMe(){
+        console.log(`UserName is ${this.username}`);
+    }
+
+    }
+
+class Teacher extends user{
+    constructor(username, email, password){
+        super(username);
+        this.email = email;
+        this.password = password;
+    }
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new user("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof user);
+
